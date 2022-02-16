756d6ccf60b285417b1080e19ac430644b229e31
Daniel <engenhariadaniel7@gmail.com>
2022-02-15 22:13:26 -0300

    Initial commit

---
+++ Virrus Scrip.py
@@ -0,0 +1,11 @@
+# Programa didático que cria um Virus.
+print("Crie o seu vírus.")
+print("_____________________________")
+print()
+x= input("Digite o nome do virus que você quer criar:  ")
+def main():
+    q = 0
+    while 1:
+        q = q +1
+        print ("virus: ", x, "Cópias:  ", q)
+main()
